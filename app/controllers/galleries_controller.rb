63e07252fdd1c8f550b00137a15dd58bc230523d
RobynSpaulding <robynm27@gmail.com>
2023-01-30 09:31:19 -0800

    fixes routes typo

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,5 @@
 class GalleriesController < ApplicationController
-  before_action :authenticate_user, except: [:index, :show]
+  # before_action :authenticate_user, except: [:index, :show]
   
   def index
     @galleries = Gallery.all
--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,23 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "galleries" => "galleries#index"
-  get "galleries/:id" => "galleries#show"
-  post "galleries" => "galleries#create"
-  patch "galleries/:id" => "galleries#update"
+  get "/galleries" => "galleries#index"
+  get "/galleries/:id" => "galleries#show"
+  post "/galleries" => "galleries#create"
+  patch "/galleries/:id" => "galleries#update"
   delete "galleries/:id" => "galleries#destroy"
 
-  get "resumes" => "resumes#index"
-  get "resumes/:id" => "resumes#show"
-  post "resumes" => "resumes#create"
-  patch "resumes/:id" => "resumes#update"
-  delete "resumes/:id" => "resumes#destroy"
+  get "/resumes" => "resumes#index"
+  get "/resumes/:id" => "resumes#show"
+  post "/resumes" => "resumes#create"
+  patch "/resumes/:id" => "resumes#update"
+  delete "/resumes/:id" => "resumes#destroy"
 
-  get "bios" => "bios#index"
-  get "bios/:id" => "bios#show"
-  post "bios" => "bios#create"
-  patch "bios/:id" => "bios#update"
-  delete "bios/:id" => "bios#destroy"
+  get "/bios" => "bios#index"
+  get "/bios/:id" => "bios#show"
+  post "/bios" => "bios#create"
+  patch "/bios/:id" => "bios#update"
+  delete "/bios/:id" => "bios#destroy"
 
   post "/users" => "users#create"
 
