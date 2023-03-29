de461fd40c5b0c78fc80d4ab18ca4e929e6ed58c
RobynSpaulding <robynm27@gmail.com>
2023-03-29 08:59:15 -0700

    fix error

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = "dqyedblov"
-  config.api_key = "278197115889761"
-  config.api_secret = "pjn9Q1Vb4hCD9XgwUSKV86txnys"
-  config.secure = true
-  config.cdn_subdomain = true
-end
